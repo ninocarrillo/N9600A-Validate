eba31842c89381cac708cbb27a3291400537e877
Nino Carrillo <nino.carrillo@outlook.com>
2024-01-20 08:38:16 -0500

    add validate-functions.py

--- n9600a-validate.py
+++ n9600a-validate.py
@@ -14,82 +14,9 @@ import sys
 import time
 import RPi.GPIO as gpio
 import subprocess
+import validate-functions as val
 
-def SetupGPIO():
-	# Test Device MODE3 Switch
-	gpio.setup(17,gpio.OUT)
-	gpio.output(17, gpio.LOW)
-	# Test Device MODE2 Switch
-	gpio.setup(18,gpio.OUT)
-	gpio.output(18,gpio.LOW)
-	# Test Device MODE1 Switch
-	gpio.setup(27,gpio.OUT)
-	gpio.output(27,gpio.LOW)
-	# Test Device MODE0 Switch
-	gpio.setup(22,gpio.OUT)
-	gpio.output(22,gpio.LOW)
-	# Test Device Test_TX Button
-	gpio.setup(23,gpio.OUT)
-	gpio.output(23,gpio.LOW)
-	# Standard Device MODE3 Switch
-	gpio.setup(24,gpio.OUT)
-	gpio.output(24,gpio.LOW)
-	# Standard Device MODE2 Switch
-	gpio.setup(25,gpio.OUT)
-	gpio.output(25,gpio.LOW)
-	# Standard Device MODE1 Switch
-	gpio.setup(5,gpio.OUT)
-	gpio.output(5,gpio.LOW)
-	# Standard Device MODE0 Switch
-	gpio.setup(6,gpio.OUT)
-	gpio.output(6,gpio.LOW)
-	return
 
-def SetTestDeviceMode(mode_pattern):
-	if mode_pattern & 8:
-		gpio.output(17,gpio.HIGH)
-	else:
-		gpio.output(17,gpio.LOW)
-	if mode_pattern & 4:
-		gpio.output(18,gpio.HIGH)
-	else:
-		gpio.output(18,gpio.LOW)
-	if mode_pattern & 2:
-		gpio.output(27,gpio.HIGH)
-	else:
-		gpio.output(27,gpio.LOW)
-	if mode_pattern & 1:
-		gpio.output(22,gpio.HIGH)
-	else:
-		gpio.output(22,gpio.LOW)
-	return
-
-def SetStandardDeviceMode(mode_pattern):
-	if mode_pattern & 8:
-		gpio.output(24,gpio.HIGH)
-	else:
-		gpio.output(24,gpio.LOW)
-	if mode_pattern & 4:
-		gpio.output(25,gpio.HIGH)
-	else:
-		gpio.output(25,gpio.LOW)
-	if mode_pattern & 2:
-		gpio.output(5,gpio.HIGH)
-	else:
-		gpio.output(5,gpio.LOW)
-	if mode_pattern & 1:
-		gpio.output(6,gpio.HIGH)
-	else:
-		gpio.output(6,gpio.LOW)
-	return
-
-def AssertTestTXButton():
-	gpio.output(23,gpio.HIGH)
-	return
-
-def ReleaseTestTXButton():
-	gpio.output(23,gpio.LOW)
-	return
 
 if sys.version_info < (3, 0):
 	print("Python version should be 3.x, exiting")
@@ -97,10 +24,10 @@ if sys.version_info < (3, 0):
 
 gpio.setmode(gpio.BCM)
 
-SetupGPIO()
+val.SetupGPIO()
 
-SetTestDeviceMode(4)
-SetStandardDeviceMode(4)
+val.SetTestDeviceMode(4)
+val.SetStandardDeviceMode(4)
 time.sleep(2)
 
 subprocess.run(["aplay", "/home/pi/github/modem-test-audio/1_single/GFSK_4800_IL2Pc_50b_1x.wav"])
@@ -108,15 +35,15 @@ subprocess.run(["aplay", "/home/pi/github/modem-test-audio/1_single/GFSK_4800_IL
 time.sleep(1)
 
 for mode in range(16):
-	SetTestDeviceMode(mode)
+	val.SetTestDeviceMode(mode)
 	print("Test Mode: ", mode)
-	SetStandardDeviceMode(mode)
+	val.SetStandardDeviceMode(mode)
 	print("Standard Mode: ", mode)
 	time.sleep(2)
 
-	AssertTestTXButton()
+	val.AssertTestTXButton()
 	time.sleep(.1)
-	ReleaseTestTXButton()
+	val.ReleaseTestTXButton()
 	time.sleep(5)
 gpio.cleanup()
 
---
+++ validate-functions.py
@@ -0,0 +1,81 @@
+# validate-functions.py
+# Python3
+# Support n9600a-validate.py
+# Nino Carrillo
+# 20 Jan 2024
+
+def SetupGPIO():
+	# Test Device MODE3 Switch
+	gpio.setup(17,gpio.OUT)
+	gpio.output(17, gpio.LOW)
+	# Test Device MODE2 Switch
+	gpio.setup(18,gpio.OUT)
+	gpio.output(18,gpio.LOW)
+	# Test Device MODE1 Switch
+	gpio.setup(27,gpio.OUT)
+	gpio.output(27,gpio.LOW)
+	# Test Device MODE0 Switch
+	gpio.setup(22,gpio.OUT)
+	gpio.output(22,gpio.LOW)
+	# Test Device Test_TX Button
+	gpio.setup(23,gpio.OUT)
+	gpio.output(23,gpio.LOW)
+	# Standard Device MODE3 Switch
+	gpio.setup(24,gpio.OUT)
+	gpio.output(24,gpio.LOW)
+	# Standard Device MODE2 Switch
+	gpio.setup(25,gpio.OUT)
+	gpio.output(25,gpio.LOW)
+	# Standard Device MODE1 Switch
+	gpio.setup(5,gpio.OUT)
+	gpio.output(5,gpio.LOW)
+	# Standard Device MODE0 Switch
+	gpio.setup(6,gpio.OUT)
+	gpio.output(6,gpio.LOW)
+	return
+
+def SetTestDeviceMode(mode_pattern):
+	if mode_pattern & 8:
+		gpio.output(17,gpio.HIGH)
+	else:
+		gpio.output(17,gpio.LOW)
+	if mode_pattern & 4:
+		gpio.output(18,gpio.HIGH)
+	else:
+		gpio.output(18,gpio.LOW)
+	if mode_pattern & 2:
+		gpio.output(27,gpio.HIGH)
+	else:
+		gpio.output(27,gpio.LOW)
+	if mode_pattern & 1:
+		gpio.output(22,gpio.HIGH)
+	else:
+		gpio.output(22,gpio.LOW)
+	return
+
+def SetStandardDeviceMode(mode_pattern):
+	if mode_pattern & 8:
+		gpio.output(24,gpio.HIGH)
+	else:
+		gpio.output(24,gpio.LOW)
+	if mode_pattern & 4:
+		gpio.output(25,gpio.HIGH)
+	else:
+		gpio.output(25,gpio.LOW)
+	if mode_pattern & 2:
+		gpio.output(5,gpio.HIGH)
+	else:
+		gpio.output(5,gpio.LOW)
+	if mode_pattern & 1:
+		gpio.output(6,gpio.HIGH)
+	else:
+		gpio.output(6,gpio.LOW)
+	return
+
+def AssertTestTXButton():
+	gpio.output(23,gpio.HIGH)
+	return
+
+def ReleaseTestTXButton():
+	gpio.output(23,gpio.LOW)
+	return
