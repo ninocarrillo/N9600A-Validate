d650c6cc6af6d19be0e62cf3c599510083506051
Nino Carrillo <nino.carrillo@outlook.com>
2024-01-20 02:50:59 +0000

    add wav playback

--- n9600a-validate.py
+++ n9600a-validate.py
@@ -13,7 +13,7 @@ import serial
 import sys
 import time
 import RPi.GPIO as gpio
-
+import subprocess
 
 def SetupGPIO():
 	# Test Device MODE3 Switch
@@ -98,6 +98,14 @@ if sys.version_info < (3, 0):
 gpio.setmode(gpio.BCM)
 
 SetupGPIO()
+
+SetTestDeviceMode(4)
+SetStandardDeviceMode(4)
+time.sleep(2)
+
+subprocess.run(["aplay", "/home/pi/github/modem-test-audio/1_single/GFSK_4800_IL2Pc_50b_1x.wav"])
+
+time.sleep(1)
 
 for mode in range(16):
 	SetTestDeviceMode(mode)
