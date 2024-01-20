1da0e80d48329e36edbdd9860071c93f0bc92086
Nino Carrillo <nino.carrillo@outlook.com>
2024-01-20 09:13:25 -0500

    update threading

--- validate_threading_functions.py
+++ validate_threading_functions.py
@@ -8,10 +8,21 @@
 import threading
 import subprocess
 
+
+# from https://stackoverflow.com/questions/2581817/python-subprocess-callback-when-cmd-exits
 def popen_and_call(on_exit, popen_args):
+    """
+    Runs the given args in a subprocess.Popen, and then calls the function
+    on_exit when the subprocess completes.
+    on_exit is a callable object, and popen_args is a list/tuple of args that 
+    would give to subprocess.Popen.
+    """
 	def run_in_thread(on_exit, popen_args):
 		proc = subprocess.Popoen(*popen_args)
 		proc.wait()
 		on_exit()
-		thread.start
+		return
+	thread = threading.Thread(target=run_in_thread, args=(on_exit, popen_args))
+	thread.start()
+	# returns immediately after the thread starts
 	return thread
